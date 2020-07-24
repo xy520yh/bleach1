17870ee36fcf256049ad487e6637312fa74dffca
xy520yh <705719648@qq.com>
2020-07-24 14:49:26 +0800

    modify

--- js/qiehuan.js
+++ js/qiehuan.js
@@ -18,5 +18,12 @@ if (/Android (\d+\.\d+)/.test(ua)) {
 }
 
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
 
 
